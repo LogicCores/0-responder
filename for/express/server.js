9f097dcb6f3163473ec8cc5ed929135763d1e89a
Christoph Dorn <christoph@christophdorn.com>
2015-09-01 23:14:11 -0700

    wip

--- for/express/server.js
+++ for/express/server.js
@@ -8,6 +8,7 @@ const MORGAN = require('morgan');
 const SEND = require('send');
 const PATH = require("path");
 const FS = require("fs");
+const COOKIES = require("cookies");
 
 
 const CONFIG = {
@@ -18,6 +19,235 @@ const CONFIG = {
 
 exports.main = function () {
 
+    var environmentConfig = require(
+        "../../../../../" + PATH.basename(PATH.join(__dirname, "../../../..")) + ".profile.json"
+    ).environments["127.0.0.1:8090"];
+
+
+    var app = new EXPRESS();
+
+    app.use(MORGAN("combined", {
+        skip: function (req, res) {
+            return res.statusCode < 400;
+        }
+    }));
+    
+    app.use(COMPRESSION());
+
+    app.use(function (req, res, next) {
+    
+        req.cookies = new COOKIES(req, res);
+
+		// Enable or disable test mode.
+		if (
+			req.query &&
+			typeof req.query.PREVIEW_KEY !== "undefined" &&
+			environmentConfig["0"] &&
+			environmentConfig["0"]["/cores/responder/for/express"] &&
+			environmentConfig["0"]["/cores/responder/for/express"].previewKey
+		) {
+			if (req.query.PREVIEW_KEY === environmentConfig["0"]["/cores/responder/for/express"].previewKey) {
+				req.cookies.set("PREVIEW_KEY", environmentConfig["0"]["/cores/responder/for/express"].previewKey);
+			} else {
+				req.cookies.set("PREVIEW_KEY", "");
+			}
+		}
+		return next();
+    });
+    
+    app.use(function (req, res, next) {
+    	var origin = null;
+        if (req.headers.origin) {
+            origin = req.headers.origin;
+        } else
+        if (req.headers.host) {
+            origin = [
+                (CONFIG.port === 443) ? "https" : "http",
+                "://",
+                req.headers.host
+            ].join("");
+        }
+        res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Cookie");
+        if (req.method === "OPTIONS") {
+            return res.end();
+        }
+        return next();
+    });
+    
+    app.use(BODY_PARSER.urlencoded({
+        extended: true
+    }));
+    app.use(BODY_PARSER.json({
+    	type: [
+    		'application/json',
+    		'application/vnd.api+json'
+    	]
+    }));
+
+
+
+    app.get(
+        /^\/cores\/export\/for\/babel\/(.*)/,
+        require("../../../../cores/export/for/babel").app({
+            basePath: PATH.join(__dirname, "../../../../www/0")
+        })
+    );
+
+    app.get(
+        /^\/cores\/export\/for\/browserify\/(.*)/,
+        require("../../../../cores/export/for/browserify").app({
+            basePath: PATH.join(__dirname, "../../../../www/0"),
+            distPath: PATH.join(__dirname, "../../../../www/0/dist"),
+        })
+    );
+
+    app.get(
+        /^\/cores\/transform\/for\/marked\/(.*)/,
+        require("../../../../cores/transform/for/marked").app({
+            basePath: PATH.join(__dirname, "../../../../data/0")
+        })
+    );
+
+    app.get(
+        /^\/cores\/auth\/for\/passport\/0(\/.*)/,
+        require("../../../../cores/auth/0-server.api").adapters.passport.app(
+            environmentConfig["0"]["/cores/auth/for/passport"]
+        )
+    );
+
+    app.get(
+        /^\/cores\/proxy\/for\/smi.cache\/travis-ci\.org\/(.*)/,
+        require("../../../../cores/proxy/0-proxy.api").adapters["smi.cache"].app({
+            cacheBasePath: PATH.join(__dirname, "../../../../cache"),
+            sourceBaseUrl: "https://travis-ci.org/"
+        })
+    );
+
+    app.get(/^\/cores\/skin\/for\/semantic-ui\/(.*)/, function (req, res, next) {
+    	return SEND(req, req.params[0], {
+    		root: PATH.join(__dirname, "../../../../cores/skin/for/semantic-ui/node_modules/semantic-ui-css")
+    	}).on("error", next).pipe(res);
+    });
+
+    app.get(/^(.*\.(?:css|png|jpg|jpeg|js|ico|font))/, function (req, res, next) {
+        return SEND(req, req.params[0], {
+    		root: PATH.join(__dirname, "../../../../www/0")
+    	}).on("error", function (err) {
+
+    	    return SEND(req, req.params[0], {
+        		root: PATH.join(__dirname, "../../../../skins/0/SemanticUI")
+        	}).on("error", next).pipe(res);
+    	}).pipe(res);
+    });
+
+    app.get(/^(.*)/, function (req, res, next) {
+
+        var htmRequested = /\.html?$/.test(req.params[0]);
+
+        if (
+            !htmRequested &&
+            !req.cookies.get("PREVIEW_KEY")
+        ) {
+            res.writeHead(403);
+            res.end("Forbidden");
+            return;
+        }
+
+        function locateFile (uri) {
+            if (/\/$/.test(uri)) {
+                uri += "index";
+            }
+
+            // TODO: Cache in dist path and use it instead of re-generating if no source modified
+            var htmExt = ".htm" + (/\/index$/.test(uri) ? "l":"");
+            var path = PATH.join(__dirname, "../../../../skins/0/SemanticUI", uri + htmExt);
+            return FS.exists(path, function (exists) {
+
+                function send (uri) {
+
+                    var path = PATH.join(__dirname, "../../../../skins/0/SemanticUI", uri);
+
+                    if (htmRequested) {
+                        // The route was requested with the 'htm' extension so we serve
+                        // the raw file instead of the componentifed file.
+                        // TODO: Remove this option and return 404 in production.
+
+                        // TODO: Relocate this into adapter.
+                        // We convert the namespaced 'component' attributes as jQuery has a hard
+                        // time selcting attributes with colons in it. Likely true for many other
+                        // parsers as well.
+                        if (/;convert-to-data;/.test(req.headers["x-component-namespace"] || "")) {
+                            return FS.readFile(path, "utf8", function (err, html) {
+                                if (err) return next(err);
+                        
+    							var re = /(<|\s)component\s*:\s*([^=]+)(\s*=\s*"[^"]*"(?:\/?>|\s))/g;
+    							var m;
+    							while ( (m = re.exec(html)) ) {
+    								html = html.replace(
+    								    new RegExp(ESCAPE_REGEXP_COMPONENT(m[0]), "g"),
+    								    m[1] + "data-component-" + m[2].replace(/:/g, "-") + m[3]
+    								);
+    							}
+
+                                res.writeHead(200, {
+                                    "Content-Type": "text/html"
+                                });
+                                res.end(html);
+                                return;
+                            });
+                        }
+
+                        return FS.createReadStream(path).pipe(res);
+                    } else {
+
+                        return FS.readFile(path, "utf8", function (err, html) {
+                            if (err) return next(err);
+
+                            return require("../../../../cores/export/for/sm.hoist.VisualComponents").parseForUri(
+                                PATH.join(__dirname, "../../../../skins/0/SemanticUI/components.json"),
+                                uri,
+                                function (err, manifest) {
+                                    if (err) return next(err);
+                                    
+                                    if (
+                                        !manifest.components ||
+                                        !manifest.components.html ||
+                                        !manifest.components.html.fsHtmlPath
+                                    ) {
+                                        var err = new Error("'html' component not declared in exports for uri '" + uri + "'!");
+                                        err.code = 404;
+                                        return next(err);
+                                    }
+
+                                    res.writeHead(200, {
+                                        "Content-Type": "text/html"
+                                    });
+                                    return FS.createReadStream(manifest.components.html.fsHtmlPath).pipe(res);
+                                }
+                            );
+                        });
+                    }
+                }
+
+                if (exists) {
+                    uri = uri + htmExt;
+                } else {
+                    // If the original path is not found we fall back
+                    // to the htm file of the parent secion and let the
+                    // client load the correct page content based on
+                    // window.location.pathname
+                    return locateFile(PATH.dirname(uri));
+                }
+                return send(uri);
+            });
+        }
+        
+        return locateFile(req.params[0]);
+    });
+
     var server = HTTP.createServer(function (req, res) {
     
         function respondWithError (err) {
@@ -27,196 +257,7 @@ exports.main = function () {
     	}
     
     	try {
-    
-            var app = EXPRESS();
-            
-            app.use(MORGAN("combined", {
-                skip: function (req, res) {
-                    return res.statusCode < 400;
-                }
-            }));
-            
-            app.use(COMPRESSION());
-            
-            app.use(function (req, res, next) {
-            	var origin = null;
-                if (req.headers.origin) {
-                    origin = req.headers.origin;
-                } else
-                if (req.headers.host) {
-                    origin = [
-                        (CONFIG.port === 443) ? "https" : "http",
-                        "://",
-                        req.headers.host
-                    ].join("");
-                }
-                res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS");
-                res.setHeader("Access-Control-Allow-Credentials", "true");
-                res.setHeader("Access-Control-Allow-Origin", origin);
-                res.setHeader("Access-Control-Allow-Headers", "Content-Type, Cookie");
-                if (req.method === "OPTIONS") {
-                    return res.end();
-                }
-                return next();
-            });
-            
-            app.use(BODY_PARSER.urlencoded({
-                extended: true
-            }));
-            app.use(BODY_PARSER.json({
-            	type: [
-            		'application/json',
-            		'application/vnd.api+json'
-            	]
-            }));
-    
-    
-    
-            app.get(
-                /^\/cores\/export\/for\/babel\/(.*)/,
-                require("../../../../cores/export/for/babel").app({
-                    basePath: PATH.join(__dirname, "../../../../www/0")
-                })
-            );
-    
-            app.get(
-                /^\/cores\/export\/for\/browserify\/(.*)/,
-                require("../../../../cores/export/for/browserify").app({
-                    basePath: PATH.join(__dirname, "../../../../www/0"),
-                    distPath: PATH.join(__dirname, "../../../../www/0/dist"),
-                })
-            );
-    
-            app.get(
-                /^\/cores\/transform\/for\/marked\/(.*)/,
-                require("../../../../cores/transform/for/marked").app({
-                    basePath: PATH.join(__dirname, "../../../../data/0")
-                })
-            );
-    
-            app.get(
-                /^\/cores\/proxy\/for\/smi.cache\/travis-ci\.org\/(.*)/,
-                require("../../../../cores/proxy/0-proxy.api").adapters["smi.cache"].app({
-                    cacheBasePath: PATH.join(__dirname, "../../../../cache"),
-                    sourceBaseUrl: "https://travis-ci.org/"
-                })
-            );
-    
-            app.get(/^\/cores\/skin\/for\/semantic-ui\/(.*)/, function (req, res, next) {
-            	return SEND(req, req.params[0], {
-            		root: PATH.join(__dirname, "../../../../cores/skin/for/semantic-ui/node_modules/semantic-ui-css")
-            	}).on("error", next).pipe(res);
-            });
-    
-    
-            app.get(/^(.*\.(?:css|png|jpg|jpeg|js|ico|font))/, function (req, res, next) {
-                return SEND(req, req.params[0], {
-            		root: PATH.join(__dirname, "../../../../www/0")
-            	}).on("error", function (err) {
 
-            	    return SEND(req, req.params[0], {
-                		root: PATH.join(__dirname, "../../../../skins/0/SemanticUI")
-                	}).on("error", next).pipe(res);
-            	}).pipe(res);
-            });
-    
-            app.get(/^(.*)/, function (req, res, next) {
-    
-                var htmRequested = /\.html?$/.test(req.params[0]);
-                
-                function locateFile (uri) {
-                    if (/\/$/.test(uri)) {
-                        uri += "index";
-                    }
-    
-                    // TODO: Cache in dist path and use it instead of re-generating if no source modified
-                    var htmExt = ".htm" + (/\/index$/.test(uri) ? "l":"");
-                    var path = PATH.join(__dirname, "../../../../skins/0/SemanticUI", uri + htmExt);
-                    return FS.exists(path, function (exists) {
-    
-                        function send (uri) {
-    
-                            var path = PATH.join(__dirname, "../../../../skins/0/SemanticUI", uri);
-    
-                            if (htmRequested) {
-                                // The route was requested with the 'htm' extension so we serve
-                                // the raw file instead of the componentifed file.
-                                // TODO: Remove this option and return 404 in production.
-    
-                                // TODO: Relocate this into adapter.
-                                // We convert the namespaced 'component' attributes as jQuery has a hard
-                                // time selcting attributes with colons in it. Likely true for many other
-                                // parsers as well.
-                                if (/;convert-to-data;/.test(req.headers["x-component-namespace"] || "")) {
-                                    return FS.readFile(path, "utf8", function (err, html) {
-                                        if (err) return next(err);
-                                
-            							var re = /(<|\s)component\s*:\s*([^=]+)(\s*=\s*"[^"]*"(?:\/?>|\s))/g;
-            							var m;
-            							while ( (m = re.exec(html)) ) {
-            								html = html.replace(
-            								    new RegExp(ESCAPE_REGEXP_COMPONENT(m[0]), "g"),
-            								    m[1] + "data-component-" + m[2].replace(/:/g, "-") + m[3]
-            								);
-            							}
-    
-                                        res.writeHead(200, {
-                                            "Content-Type": "text/html"
-                                        });
-                                        res.end(html);
-                                        return;
-                                    });
-                                }
-    
-                                return FS.createReadStream(path).pipe(res);
-                            } else {
-    
-                                return FS.readFile(path, "utf8", function (err, html) {
-                                    if (err) return next(err);
-        
-                                    return require("../../../../cores/export/for/sm.hoist.VisualComponents").parseForUri(
-                                        PATH.join(__dirname, "../../../../skins/0/SemanticUI/components.json"),
-                                        uri,
-                                        function (err, manifest) {
-                                            if (err) return next(err);
-                                            
-                                            if (
-                                                !manifest.components ||
-                                                !manifest.components.html ||
-                                                !manifest.components.html.fsHtmlPath
-                                            ) {
-                                                var err = new Error("'html' component not declared in exports for uri '" + uri + "'!");
-                                                err.code = 404;
-                                                return next(err);
-                                            }
-
-                                            res.writeHead(200, {
-                                                "Content-Type": "text/html"
-                                            });
-                                            return FS.createReadStream(manifest.components.html.fsHtmlPath).pipe(res);
-                                        }
-                                    );
-                                });
-                            }
-                        }
-    
-                        if (exists) {
-                            uri = uri + htmExt;
-                        } else {
-                            // If the original path is not found we fall back
-                            // to the htm file of the parent secion and let the
-                            // client load the correct page content based on
-                            // window.location.pathname
-                            return locateFile(PATH.dirname(uri));
-                        }
-                        return send(uri);
-                    });
-                }
-                
-                return locateFile(req.params[0]);
-            });
-    
-    
     		return app(req, res, respondWithError);
     
     	} catch (err) {
